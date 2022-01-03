31c0f7bc08affeaab2f7141cbcb8807e1a105c24
Siddhesh Mangela <sidthecool007@gmail.com>
2022-01-03 11:38:23 +0530

    account added

---
+++ store-app-svelte/src/lib/IconButtonLink.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let href = "";
+  export let alt = "";
+</script>
+
+<a class="icon-button-link" {href} {alt}>
+  <slot />
+</a>
+
+<style lang="scss">
+  .icon-button-link {
+    color: var(--color-font-dark);
+    width: 30px;
+    height: 30px;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 4px;
+    overflow: hidden;
+
+    :global(svg) {
+      width: 1.8rem;
+      height: 1.8rem;
+    }
+  }
+</style>
